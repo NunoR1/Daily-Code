c82cf897fd75294354990587ce4d7d998bf4a162
Nuno <61714135+NunoR1@users.noreply.github.com>
2025-02-07 09:04:23 -0500

    LAMBDAS

--- Java/DOIT.java
+++ Java/DOIT.java
@@ -9,10 +9,10 @@ public class DOIT {
             System.out.print("Input a positive integer: ");
             number = scanner.nextInt();
             
-            if (number >= 0) {
+            if (number > 0) {
                 System.out.println("You entered: " + number);
             }
-        } while (number < 0);
+        } while (number <= 0);
 
         scanner.close();
     }
